3b624287d4b36fa53b10337c342ea4c59762ff04
sebastianorellana017 <sebastianorellana017@gmail.com>
2022-01-04 05:35:15 -0300

    categories in orden -1

--- web/miapp/models.py
+++ web/miapp/models.py
@@ -25,6 +25,7 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Articulo'
         verbose_name_plural = 'Articulos'
+        ordering = ["-id"]
 
     def __str__(self):
         return self.title
